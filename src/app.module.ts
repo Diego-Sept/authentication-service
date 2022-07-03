b5d3d77223427eb04873e58da799b25dccc1dd80
Diego Sept <dsept@infosis.com.ar>
2022-07-03 18:13:11 -0300

    JWT initial configuration and install typeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,26 @@ import { RegisterModule } from './register/register.module';
 import { LoginModule } from './login/login.module';
 import { LogoutModule } from './logout/logout.module';
 import { ProjectsModule } from './projects/projects.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [RegisterModule, LoginModule, LogoutModule, ProjectsModule],
+  imports: [
+    RegisterModule, 
+    LoginModule, 
+    LogoutModule, 
+    ProjectsModule, 
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '',
+      port: 3306,
+      username: '',
+      password: '',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class LoginService {}
+export class LoginService {
+
+    constructor(
+        private usersService: UsersService,
+        private jwtTokenService: JwtService
+    ){}
+
+    async validateUserCredentials(username: string, password: string): Promise<any> {
+        const user = await this.usersService.findOne(username);
+
+        if (user && user.password === password) {
+            const {password, ...result} = user;
+            return result;
+        }
+        return null;
+    }
+
+    async loginWithCredentials(user: any) {
+        const payload = { username: user.username, sub: user.userId };
+
+        return {
+            access_token: this.jwtTokenService.sign(payload),
+        };
+    }
+
+}
